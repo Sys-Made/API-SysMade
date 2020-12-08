69ecd9beeebebd02cb1cb961b9f88fb1cea3e41b
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2020-12-08 14:40:19 -0300

    Update ActivityCadastraSocio.java

--- app/src/main/java/com/example/api_sysmade/ActivityCadastraSocio.java
+++ app/src/main/java/com/example/api_sysmade/ActivityCadastraSocio.java
@@ -2,13 +2,28 @@ package com.example.api_sysmade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityCadastraSocio extends AppCompatActivity {
-
+    Button buttonVoltarCadSocio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_socio);
+        buttonVoltarCadSocio = findViewById(R.id.buttonVoltarCadSocio);
+        voltaMenuSocio();
+    }
+
+    private void voltaMenuSocio(){
+        buttonVoltarCadSocio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaMenuSocio = new Intent(getApplicationContext(), ActivityTelaSocioMenu.class);
+                startActivity(telaMenuSocio);
+            }
+        });
     }
 }
